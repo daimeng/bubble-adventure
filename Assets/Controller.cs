36796642551d6aeb6fb337a754e793161dda6370
Daimeng Chen <daimengchen@gmail.com>
2025-01-25 16:16:47 +0800

    spawn bubbles

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public Transform guy;
+    float cursor = 0;
+
     public GameObject bubblePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(bubblePrefab, Vector3.zero, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (cursor > guy.transform.position.x + 10) return;
+
+        var rng = Random.Range(0, 100);
+        if (rng < 10)
+        {
+            Instantiate(bubblePrefab, new Vector3(cursor, rng, 0), Quaternion.identity);
+            cursor += 2;
+        }
+
+        cursor += 0.1f;
 
     }
 }
