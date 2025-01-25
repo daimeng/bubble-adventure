e2ab88d62d385ea1f46b7153a7fe8c19706f0459
Daimeng Chen <daimengchen@gmail.com>
2025-01-25 14:25:42 +0800

    drag line

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,6 +13,10 @@ public class Movement : MonoBehaviour
     private Vector2 mousePos = Vector2.zero;
     private bool isDragging = false;
 
+    public Bubble bubble = null;
+
+    public bool controlled = true;
+
     public float maxDragDist = 3f;
     public float launchForceMult = 10f;
 
@@ -20,36 +25,47 @@ public class Movement : MonoBehaviour
     public LayerMask groundLayer; // LayerMask to detect ground
     public Transform groundCheck; // Empty GameObject to check if the player is grounded
     public float groundCheckRadius = 0.5f; // Radius for ground check
-
-    public Bubble bubble = null;
-
-    public bool controlled = true;
-
+    public Transform DrawLine;
+    private SpriteRenderer drawline;
     private Rigidbody2D rb;
     private bool isGrounded;
 
-    // private PlayerInputManager inputs;
-
     void Start()
     {
-        // inputs = new PlayerInputManager();
+        drawline = DrawLine.GetComponent<SpriteRenderer>();
+        drawline.enabled = false;
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // void OnCollisionEnter2D(Collision2D c)
-    // {
-    //     if (c.gameObject.layer == 6)
-    //     {
-    //         isGrounded = true;
-    //     }
-    // }
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.layer == 6)
+        {
+            isGrounded = true;
+        }
+    }
 
     void FixedUpdate()
     {
+        // Apply some friction on Ground to stop sliding
+        if (isGrounded)
+        {
+            if (Math.Abs(rb.linearVelocityX) < 0.5)
+            {
+                rb.linearVelocityX = 0;
+            }
+            else
+            {
+                rb.linearVelocityX *= 0.9f;
+            }
+        }
+
         if (bubble == null) return;
 
+        // If in bubble, suck to center
         var d = (bubble.transform.position - transform.position) / 2;
-        var nextpos = transform.position + d;
+        var clamped = Math.Min(d.magnitude, 0.1f);
+        var nextpos = transform.position + d.normalized * clamped;
         if (d.sqrMagnitude < 0.1)
         {
             rb.MovePosition(bubble.transform.position);
@@ -66,6 +82,7 @@ public class Movement : MonoBehaviour
     {
         if (!controlled) return;
 
+        // Drag Controls
         if (Input.GetMouseButtonDown(0)) // Left mouse button pressed
         {
             // Check if the mouse is over the object
@@ -75,15 +92,26 @@ public class Movement : MonoBehaviour
 
             if (hit.collider != null && hit.collider.gameObject == gameObject)
             {
+                drawline.enabled = true;
+                dragStart = mousePos;
                 isDragging = true;
                 // rb.isKinematic = true; // Disable physics while dragging
-                dragStart = mousePos;
             }
         }
 
         if (isDragging)
         {
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 dragDirection = dragStart - mousePos;
+            dragDirection = Vector2.ClampMagnitude(dragDirection, maxDragDist);
+            // Convert the direction to a Quaternion rotation
+            float angle = Mathf.Atan2(dragDirection.y, dragDirection.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.Euler(0, 0, angle);
+
+            // Apply the rotation to the object
+            DrawLine.transform.SetPositionAndRotation(transform.position, rotation);
+            DrawLine.localScale = new Vector3(dragDirection.magnitude, 0.1f, 1);
+
         }
 
         if (Input.GetMouseButtonUp(0)) // Left mouse button released
@@ -91,6 +119,7 @@ public class Movement : MonoBehaviour
             if (!isDragging) return;
 
             isDragging = false;
+            drawline.enabled = false;
 
             Vector2 dragDirection = dragStart - mousePos;
             dragDirection = Vector2.ClampMagnitude(dragDirection, maxDragDist);
@@ -100,6 +129,7 @@ public class Movement : MonoBehaviour
             var mod = (float)Math.Log(f.sqrMagnitude) / 2;
             rb.AddForce(f.normalized * mod * launchForceMult, ForceMode2D.Impulse);
             rb.gravityScale = 1;
+            isGrounded = false;
         }
     }
 
@@ -110,12 +140,6 @@ public class Movement : MonoBehaviour
         {
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(transform.position, groundCheckRadius);
-        }
-
-        if (isDragging)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(dragStart, mousePos);
         }
     }
 }
